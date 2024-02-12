35d0af24be6390991187fc92bee912a70c5230d4
Francisco Santamaria <sm19035@ues.edu.sv>
2024-02-11 22:31:22 -0600

    cambios

--- src/presentation/todos/routes.ts
+++ src/presentation/todos/routes.ts
@@ -4,13 +4,13 @@ import { TodosController } from "./controller";
 export class TodosRoutes {
   static get routes(): Router {
     const router = Router();
-    const todoController=new TodosController();
+    const todoController = new TodosController();
 
-    router.get("/",todoController.getTodos);
-    router.get("/:id",todoController.getTodoById);
-    router.post("/",todoController.createTodo);
-    router.put("/:id",todoController.updateTodo);
-    router.delete("/:id",todoController.deleteTodo);
+    router.get("/", todoController.getTodos);
+    router.get("/:id", todoController.getTodoById);
+    router.post("/", todoController.createTodo);
+    router.put("/:id", todoController.updateTodo);
+    router.delete("/:id", todoController.deleteTodo);
     return router;
   }
 }
